4d26dd24554494702c7d59cd197be5b86898dfbe
ramesh432 <rameshkondru999@gmail.com>
2020-11-20 12:23:31 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -5,19 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>ram</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>ramesh</h1>
       </header>
     </div>
   );
